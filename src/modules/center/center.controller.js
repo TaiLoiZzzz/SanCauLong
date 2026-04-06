17f91223556cc6f1de03a142f9aba347696215b3
TaiLoiZzzz <tailoi1606@gmail.com>
2026-04-06 16:57:05 +0700

    feature/validationServiceCourtCenter

--- src/modules/center/center.controller.js
+++ src/modules/center/center.controller.js
@@ -1,1 +1,41 @@
-const centerService = require("../center/center.service")
+const centerService = require('./center.service');
+
+const getCenters = async (req, res, next) => {
+  try {
+    const data = await centerService.getAllCenters();
+    res.status(200).json({ success: true, data });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getCourts = async (req, res, next) => {
+  try {
+    const { centerId } = req.params;
+    const data = await centerService.getCourtsByCenter(centerId);
+    res.status(200).json({ success: true, data });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const postCenter = async (req, res, next) => {
+  try {
+    const data = await centerService.createCenter(req.body);
+    res.status(201).json({ success: true, data });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const postCourt = async (req, res, next) => {
+  try {
+    const { centerId } = req.params;
+    const data = await centerService.createCourt(centerId, req.body);
+    res.status(201).json({ success: true, data });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { getCenters, getCourts, postCenter, postCourt };
